ee3ccf4d8a662697728d6e24cf34f2420d301958
srobertsphd <samantha.roberts@gmail.com>
2023-04-30 22:35:21 -0400

    added last row and new secondary color

--- src/script.js
+++ src/script.js
@@ -6,7 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log(buttons);
 
   let currentValue = "";
- 
+  let prevValue = "";
+  let isRad = true; //default
+  let isDeg = false; //default
+  let isInv = false; //default
 
   function evaluateResult() {
     console.log("currentValue", currentValue);
@@ -14,20 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
       .replace("×", "*")
       .replace("÷", "/")
       .replace("%", "*0.01")
-      .replace("sin", 'Math.sin')
-      .replace("cos", 'Math.cos')
-      .replace("π", 'Math.PI')
-      .replace("ln", 'Math.log')
-      .replace("√", 'Math.sqrt')
-      .replace("^", '**')
-      .replace("log", 'Math.log10')
-      .replace("EXP", 'Math.exp')
-      .replace("e", 'Math.E');
-          
+      .replace("sin", "Math.sin")
+      .replace("cos", "Math.cos")
+      .replace("π", "Math.PI")
+      .replace("log", "Math.log10") // do NOT put this below the "ln" or log will screw up
+      .replace("ln", "Math.log")
+      .replace("√", "Math.sqrt")
+      .replace("^", "**")
+      //   .replace("EXP", 'Math.exp')
+      .replace("e", "Math.E");
+
     console.log("convertedValue", convertedValue);
     const result = eval(convertedValue);
     currentValue = result.toString();
     display.value = currentValue;
+    prevValue = currentValue;
+    currentValue = "";
   }
 
   for (let i = 0; i < buttons.length; i++) {
@@ -44,6 +49,29 @@ document.addEventListener("DOMContentLoaded", function () {
         let base = display.value;
         display.value = base + "^";
         currentValue = display.value;
+      } else if (value == "EXP") {
+        let base = display.value;
+        display.value = base + "*10^";
+        currentValue = display.value;
+      } else if (value == "Ans") {
+        display.value = "Ans";
+        currentValue = prevValue;
+      } else if (value == "Rad") {
+        if (button.style.fontWeight === "bold") {
+          button.style.fontWeight = "normal";
+          isRad = false;
+        } else {
+          button.style.fontWeight = "bold";
+          isRad = true;
+        }
+     } else if (value == "Deg") {
+        if (button.style.fontWeight === "bold") {
+          button.style.fontWeight = "normal";
+          isDeg = false;
+        } else {
+          button.style.fontWeight = "bold";
+          isDeg = true;
+        }
       } else {
         currentValue += value;
         display.value = currentValue;
